1e3283dad425fb6f77e7d8d867e6aa8bfea9cc60
musukeshu <muskan20031998@gmail.com>
2020-07-04 22:52:35 +0530

    Adding the files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**First Slider */
+$(".slider-one").slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    variableWidth: true
+});
